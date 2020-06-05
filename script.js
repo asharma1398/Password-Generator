fbb7fc83d753c3b3911a715113ea170fc7be8a69
asharma1398 <asharma1398@gmail.com>
2020-06-05 15:36:39 -0400

    user can select four other password character options

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ function generatePassword() {
   var special = ['@', '%', '+', '', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
   var numerical = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  // "prompt" the user to state desired password length - this prompt is of type 'string'
+  var possibleChar = [];
+
+  // "prompt" the user to state desired password length - this prompt answer is of type 'string'
   var userLength = prompt("How many characters would you like your password to contain?");
   
-  // if the user inputs a value other than int (ex: letter, decimal, special character), "alert" the user of an input error
+  // if the user inputs a value other than int (ex: letter, decimal, special character), "alert" the user with an input error message
   if ( Math.round(Number(userLength)) !== Number(userLength) ) {
     alert("Error: Please enter an integer value");
   } 
-  // if the user enters a number great than 128 or less than 8, "alert" the user of an input error 
+  // if the user enters a number great than 128 or less than 8, "alert" the user with an input error message
   else {
     if (Number(userLength) < 8) {
       alert("Error: Password length must be at least 8 characters");
@@ -27,6 +29,32 @@ function generatePassword() {
     } 
     // user goes through four other password character options:
     else {
+      var usingUpper = confirm("Click OK to confirm including uppercase characters");
+      var usingLower = confirm("Click OK to confirm including lowercase characters");
+      var usingNumerical = confirm("Click OK to confirm including numerical characters");
+      var usingSpecial = confirm("Click OK to confirm including special characters");
+
+      // if the user doesn't select any of the four character types, "alert" the user with a selection error message
+      if (usingUpper == false && usingLower == false && usingNumerical == false && usingSpecial == false) {
+        alert("Error: At least one character type must be selected")
+      } 
+      // concatenate the wanted character type(s) into possibleChar array
+      else {
+        if (usingUpper) {
+          var possibleChar = possibleChar.concat(uppercase)
+        }
+        if (usingLower) {
+          var possibleChar = possibleChar.concat(lowercase)
+        }
+        if (usingNumerical) {
+          var possibleChar = possibleChar.concat(numerical)
+        }
+        if (usingSpecial) {
+          var possibleChar = possibleChar.concat(special)
+        }
+      }
+      
+      
     }
   }
 
@@ -48,13 +76,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // PASSWORD-GENERATOR LOGIC
 
-// user goes through four other password options:
-// Click OK to confirm including special characters 
-// Click OK to confirm including numerical characters 
-// Click OK to confirm including lowercase characters 
-// Click OK to confirm including upercase characters 
-
-// for every type of character accepted by the user, concatenate previously defined variables into one array
 // then perform random generation of Password based on user length chosen
 // if the user does not hit OK to one of the following 4 character options, "alert" the user they must select one of the following characters to generate a password: special, numerical, lowercase, or upercase. 
 
