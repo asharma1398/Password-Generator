1b4366a68a2429afb91b9a782636023c8bb40254
asharma1398 <asharma1398@gmail.com>
2020-06-05 13:05:29 -0400

    password character variables created and user selected password length options outlined

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // write generatePassword function here 
 
+function generatePassword() {
+
+  // all possible character options for password
+  var special = ['@', '%', '+', '', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var numerical = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  // "prompt" the user to state desired password length - this prompt is of type 'string'
+  var userLength = prompt("How many characters would you like your password to contain?");
+  
+  // if the user inputs a value other than int (ex: letter, decimal, special character), "alert" the user of an input error
+  if ( Math.round(Number(userLength)) !== Number(userLength) ) {
+    alert("Error: Please enter an integer value");
+  } 
+  // if the user enters a number great than 128 or less than 8, "alert" the user of an input error 
+  else {
+    if (Number(userLength) < 8) {
+      alert("Error: Password length must be at least 8 characters");
+    } else if (Number(userLength) >128) {
+      alert("Error: Password length must be no more than 128 characters");
+    } 
+    // user goes through four other password character options:
+    else {
+    }
+  }
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,13 +48,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // PASSWORD-GENERATOR LOGIC
 
-// "alert" the user to click the button on the screen to generate a new secure password 
-
-// "prompt" the user to state desired password length 
-// if the user inputs a value other than int, "alert" the user to input correct value 
-// if user enters a number less than 8 characters, "alert" them that the Password length must be at least 8 characters 
-// if user enters a number more than 128 characters, "alert" them that the Password length must be no more than 128 characters 
-
 // user goes through four other password options:
 // Click OK to confirm including special characters 
 // Click OK to confirm including numerical characters 
@@ -32,8 +55,8 @@ generateBtn.addEventListener("click", writePassword);
 // Click OK to confirm including upercase characters 
 
 // for every type of character accepted by the user, concatenate previously defined variables into one array
-// then perform random generation of Password base on user length chosen
-// if the user does not hit OK to one of the following "alert" the user they must select one of the following characters to generate a password: special, numerical, lowercase, or upercase. 
+// then perform random generation of Password based on user length chosen
+// if the user does not hit OK to one of the following 4 character options, "alert" the user they must select one of the following characters to generate a password: special, numerical, lowercase, or upercase. 
 
 // display generated password as an alert or write it to the page 
 
