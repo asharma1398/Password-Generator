d578203398f604a4bed7d2c9b4504853b8e4c377
asharma1398 <asharma1398@gmail.com>
2020-06-07 08:40:21 -0400

    escape key error fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   // all possible character options for password
-  var special = ['@', '%', '+', '"', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
   var numerical = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -47,22 +47,22 @@ function generatePassword() {
       else {
         if (usingUpper) {
           var possibleChar = possibleChar.concat(uppercase);
-          var upper = " Uppercase "
+          var upper = " Uppercase"
           var charIncluded = charIncluded.concat(upper)
         }
         if (usingLower) {
           var possibleChar = possibleChar.concat(lowercase);
-          var lower = " Lowercase "
+          var lower = " Lowercase"
           var charIncluded = charIncluded.concat(lower)
         }
         if (usingNumerical) {
           var possibleChar = possibleChar.concat(numerical);
-          var num = " Numerical "
+          var num = " Numerical"
           var charIncluded = charIncluded.concat(num)
         }
         if (usingSpecial) {
           var possibleChar = possibleChar.concat(special);
-          var spec = " Special "
+          var spec = " Special"
           var charIncluded = charIncluded.concat(spec)
           
         }
@@ -77,7 +77,7 @@ function generatePassword() {
   }
 
   // alert user of new password and filters used
-  alert("Your New Password: " + password + "\n\nPassword Length: " + userLength + "\nIncluded Characters:" + charIncluded)
+  alert("Your New Password: " + password + "\n\nPassword Length: " + password.length + "\nIncluded Characters:" + charIncluded)
 }
 
 // Write password to the #password input
